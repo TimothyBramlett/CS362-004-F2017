fed7caf6242c53e9d6eaf829168c0ddabc754ca8
Timothy Bramlett <tim@TimMBP.local>
2017-11-15 16:31:01 -0600

    adding lastest files from assign 4 branch

--- projects/bramlett/dominion/dominion.h
+++ projects/bramlett/dominion/dominion.h
@@ -128,4 +128,5 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int cardEffect_adventurer(int drawntreasure, struct gameState *state, int currentPlayer, int cardDrawn, int z, int* temphand);
 #endif
