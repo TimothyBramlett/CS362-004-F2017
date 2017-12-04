d6d8ce681334376e71df400eb157c141ccb46005
lucasmurphey <lucasmurphey@gmail.com>
2017-12-04 15:35:46 -0500

    added what i think is correct partitioning

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -36,8 +36,7 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
+    
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
@@ -142,13 +141,109 @@ public class UrlValidatorTest extends TestCase {
    }
    
    
-   public void testYourFirstPartition()
-   {
+   public void testYourFirstPartition() {
 	   
+	   //This will output failed tests for valid URLs flagged as invalid
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String validUrls[] = {
+			   "http://foo.com/blah_blah",
+				"http://foo.com/blah_blah/",
+				"http://foo.com/blah_blah_(wikipedia)",
+				"http://foo.com/blah_blah_(wikipedia)_(again)",
+				"http://www.example.com/wpstyle/?p=364",
+				"https://www.example.com/foo/?bar=baz&inga=42&quux",
+				"http://✪df.ws/123",
+				"http://userid:password@example.com:8080",
+				"http://userid:password@example.com:8080/",
+				"http://userid@example.com",
+				"http://userid@example.com/",
+				"http://userid@example.com:8080",
+				"http://userid@example.com:8080/",
+				"http://userid:password@example.com",
+				"http://userid:password@example.com/",
+				"http://142.42.1.1/",
+				"http://142.42.1.1:8080/",
+				"http://➡.ws/䨹",
+				"http://⌘.ws",
+				"http://⌘.ws/",
+				"http://foo.com/blah_(wikipedia)#cite-1",
+				"http://foo.com/blah_(wikipedia)_blah#cite-1",
+				"http://foo.com/unicode_(✪)_in_parens",
+				"http://foo.com/(something)?after=parens",
+				"http://☺.damowmow.com/",
+				"http://code.google.com/events/#&product=browser",
+				"http://j.mp",
+				"ftp://foo.bar/baz",
+				"http://foo.bar/?q=Test%20URL-encoded%20stuff",
+				"http://مثال.إختبار",
+				"http://例子.测试",
+				"http://उदाहरण.परीक्षा",
+				"http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com",
+				"http://a.b-c.de",
+				"http://223.255.255.254"
+	   	};
+	   Boolean expected = true;
+	   System.out.println("\nThe following VALID URLs fail the validator:");
+	   for (int i = 0; i < validUrls.length; i++) {
+		   if (urlVal.isValid(validUrls[i]) != expected) { 
+			   System.out.println("\"" + validUrls[i] + "\"");
+		   }
+	   }	   
    }
    
    public void testYourSecondPartition(){
 	   
+	   //This will output failed tests for invalid URLs flagged as valid
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String invalidUrls[] = {
+			   	"http://-a.b.co",
+	   			"http://",
+	   			"http://.",
+	   			"http://..",
+	   			"http://../",
+	   			"http://?",
+	   			"http://??",
+	   			"http://??/",
+	   			"http://#",
+	   			"http://##",
+	   			"http://##/",
+	   			"http://foo.bar?q=Spaces should be encoded",
+	   			"//",
+	   			"//a",
+	   			"///a",
+	   			"///",
+	   			"http:///a",
+	   			"foo.com",
+	   			"rdar://1234",
+	   			"h://test",
+	   			"http:// shouldfail.com",
+	   			":// should fail",
+	   			"http://foo.bar/foo(bar)baz quux",
+	   			"ftps://foo.bar/",
+	   			"http://-error-.invalid/",
+	   			"http://a.b--c.de/",
+	   			"http://-a.b.co",
+	   			"http://a.b-.co",
+	   			"http://0.0.0.0",
+	   			"http://10.1.1.0",
+	   			"http://10.1.1.255",
+	   			"http://224.1.1.1",
+	   			"http://1.1.1.1.1",
+	   			"http://123.123.123",
+	   			"http://3628126748",
+	   			"http://.www.foo.bar/",
+	   			"http://www.foo.bar./",
+	   			"http://.www.foo.bar./",
+	   			"http://10.1.1.1",
+	   			"http://10.1.1.254"
+	   		};
+	   Boolean expected = false;
+	   System.out.println("\nThe following INVALID URLs pass the validator:");
+	   for (int i = 0; i < invalidUrls.length; i++) {
+		   if (urlVal.isValid(invalidUrls[i]) != expected) { 
+			   System.out.println("\"" + invalidUrls[i] + "\"");
+		   }
+	   }
    }
    
    
